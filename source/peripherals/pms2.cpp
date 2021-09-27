7f8bf3b80da8c8d45f32498034599a9682ab441d
Aurelio Mannara <aurelio.mannara@gmail.com>
2021-09-27 22:29:55 +0200

    Fixed PMS2 update process

--- source/peripherals/pms2.cpp
+++ source/peripherals/pms2.cpp
@@ -138,8 +138,8 @@ namespace PMS2 {
 
         //Read update file size
         fseek(fp, 0, SEEK_END);
-        u32 size = ftell(fp);
-        rewind(fp);
+        u32 size = ftell(fp) - sizeof(magic); //Skip header
+        fseek(fp, sizeof(magic), SEEK_SET); //Skip header
 
         u32 tempSize = size;
         u32 flashedSize = 0;
@@ -161,10 +161,28 @@ namespace PMS2 {
                     //Read back written flash and compare
                     //We only need to send out address for CMD_READFLASH
                     i2c_writeReadBuffer(curPMSAddress, CMD_READFLASH, tempBuffer, 3, readFlashBuffer, 3 + PGM_BLOCK_SIZE, &error);
-                    if (readFlashBuffer[0] == ERR_NONE && !memcmp(&readFlashBuffer[1], &tempBuffer[3], 2 + PGM_BLOCK_SIZE))
+                    if (readFlashBuffer[0] == ERR_NONE && !memcmp(&readFlashBuffer[1], &tempBuffer[3], 2 + PGM_BLOCK_SIZE)) {
                         flashed = true;
+                    } else {
+                        Debug("%06X: write mismatch\n", address);
+                    }
+                } else {
+                    Debug("%06X: writeFlashError: %d\n", address, writeFlashError);
                 }
             }
+
+            if (!flashed) {
+                fclose(fp);
+                Debug("PMS2 booting payload\n");
+                i2c_writeBuffer(curPMSAddress, CMD_BOOTPAYLOAD, unlockBuffer, 4, &error);
+                udelay(100000);
+                LWP_MutexLock(updateMutex);
+                updateSuccess = false;
+                updating = false;
+                LWP_MutexUnlock(updateMutex);
+                return NULL;
+            }
+
             flashedSize += PGM_BLOCK_SIZE + 2;
             tempSize -= PGM_BLOCK_SIZE + 2;
             address += PGM_BLOCK_SIZE;
@@ -181,6 +199,7 @@ namespace PMS2 {
 
         LWP_MutexLock(updateMutex);
         updating = false;
+        updateSuccess = true;
         LWP_MutexUnlock(updateMutex);
         return NULL;
     }
