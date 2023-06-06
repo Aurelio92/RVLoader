516e0431233e899c40f44df545ac40757b54e366
Aurelio92 <aurelio.mannara@gmail.com>
2023-06-06 20:13:01 +0200

    Added readFile function

--- main/include/utils.h
+++ main/include/utils.h
@@ -3,3 +3,4 @@
 #define ALIGNED(x) __attribute__((aligned(x)))
 
 bool matchStr(const char* str1, const char* str2);
+int readFile(const char* path, uint8_t** buffer, uint32_t* fileSize);
--- main/source/utils.cpp
+++ main/source/utils.cpp
@@ -1,3 +1,7 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <malloc.h>
+
 bool matchStr(const char* str1, const char* str2) {
     if (!str1 || !str2)
         return false;
@@ -10,4 +14,34 @@ bool matchStr(const char* str1, const char* str2) {
     } while (c1 && c2);
 
     return true;
+}
+
+int readFile(const char* path, uint8_t** buffer, uint32_t* fileSize) {
+    if (path == NULL || buffer == NULL) {
+        return -1;
+    }
+    FILE* fp = fopen(path, "rb");
+    if (fp == NULL) {
+        printf("Error while reading file %s\n", path);
+        return -1;
+    }
+
+    fseek(fp, 0, SEEK_END);
+    uint32_t size = ftell(fp);
+    rewind(fp);
+    *buffer = (uint8_t*)malloc(size);
+
+    if (*buffer == NULL) {
+        fclose(fp);
+        return -1;
+    }
+    
+    fread(*buffer, 1, size, fp);
+    fclose(fp);
+
+    if (fileSize != NULL) {
+        *fileSize = size;
+    }
+
+    return 0;
 }
