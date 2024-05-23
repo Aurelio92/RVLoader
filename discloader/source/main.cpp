d1a29f57e10bf75859d5a91fbcdacd785c8ded92
supertazon <abordat@gmail.com>
2024-05-24 00:03:22 +0200

    Update main.cpp
    Toggle displaying log lines depending on received command line arguments from bootDiscLoader function (in main/hiidra.cpp)

--- discloader/source/main.cpp
+++ discloader/source/main.cpp
@@ -6,6 +6,7 @@
 
 static void *xfb = NULL;
 static GXRModeObj *rmode = NULL;
+bool hideLines = false;
 
 int main(int argc, char **argv) {
     struct discHdr *header;
@@ -26,14 +27,25 @@ int main(int argc, char **argv) {
     if(rmode->viTVMode&VI_NON_INTERLACE) VIDEO_WaitVSync();
 
     printf("\x1b[2;0H");
-    printf("Disc_Init() returned: %d\n", Disc_Init());
-    header = (struct discHdr *)memalign(32, sizeof(struct discHdr));
-    while (Disc_Open() < 0);
-    //printf("Disc_Open() returned: %d\n", Disc_Open());
-    printf("Disc_IsWii() returned: %d\n", Disc_IsWii());
-    printf("Disc_ReadHeader() returned: %d\n", Disc_ReadHeader(header));
-    printf("Disc_WiiBoot() returned: %d\n", Disc_WiiBoot(0));
-
+	if(argv[1][0] == '1'){
+	    hideLines = true;
+	    Disc_Init();
+		header = (struct discHdr *)memalign(32, sizeof(struct discHdr));
+		while (Disc_Open() < 0);
+		Disc_IsWii();
+		Disc_ReadHeader(header);
+		Disc_WiiBoot(0);
+	}
+    else{
+	    printf("Disc_Init() returned: %d\n", Disc_Init());
+        header = (struct discHdr *)memalign(32, sizeof(struct discHdr));
+        while (Disc_Open() < 0);
+        //printf("Disc_Open() returned: %d\n", Disc_Open());
+        printf("Disc_IsWii() returned: %d\n", Disc_IsWii());
+        printf("Disc_ReadHeader() returned: %d\n", Disc_ReadHeader(header));
+        printf("Disc_WiiBoot() returned: %d\n", Disc_WiiBoot(0));
+    }
+	
     while(1) {
         VIDEO_WaitVSync();
     }
